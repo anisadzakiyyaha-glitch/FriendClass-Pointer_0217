605dad48cc38204fa43066fe7df5fa0732b0fd03
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-04-27 21:31:07 +0700

    Tambah fungsi inputData dan hitungLuas BelahKetupat

--- Tugas.cpp
+++ Tugas.cpp
@@ -6,6 +6,7 @@ class BelahKetupat;
 class LayangLayang {
 private:
     double d1, d2, sisi1, sisi2;
+
 public:
     void inputData() {
         cout << "Diagonal 1 : "; cin >> d1;
@@ -13,7 +14,6 @@ public:
         cout << "Sisi 1     : "; cin >> sisi1;
         cout << "Sisi 2     : "; cin >> sisi2;
     }
-};
 
     double hitungLuas() {
         return 0.5 * d1 * d2;
@@ -23,13 +23,22 @@ public:
         return 2 * (sisi1 + sisi2);
     }
 
-void outputData() {
+    void outputData() {
         cout << "Luas Layang-Layang     : " << hitungLuas() << endl;
         cout << "Keliling Layang-Layang : " << hitungKeliling() << endl;
     }
-  friend double kelilingLayang(LayangLayang &l, BelahKetupat &b);
 
-  class BelahKetupat {
-private:
+    friend double kelilingLayang(LayangLayang &l, BelahKetupat &b);
+};
+
+class BelahKetupat {
+    private:
     double sisi, d1, d2;
+
+public:
+    void inputData() {
+        cout << "Sisi       : "; cin >> sisi;
+        cout << "Diagonal 1 : "; cin >> d1;
+        cout << "Diagonal 2 : "; cin >> d2;
+    }
 };
