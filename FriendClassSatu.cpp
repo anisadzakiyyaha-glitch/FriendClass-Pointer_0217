4d81fbc8c55f4cec019aa3b03c3e44382a59b796
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-04-27 18:32:43 +0700

    buat class orang dengan atribut nama

--- FriendClassSatu.cpp
+++ FriendClassSatu.cpp
@@ -5,38 +5,9 @@ using namespace std;
 class siswa;
 class orang
 {
-private:
+    private:
 string nama;
 
 public:
 void setNama(string pNama);
-friend class siswa;
 };
-
-class siswa
-{
-private:
-    int id;
-
-public:
-    void setId(int pId);
-    void displayAll(orang &a);
-};
-
-void siswa::displayAll(orang &a)
-{
-    cout << id << endl << a.nama;
-}
-void siswa::setId(int pId)
-{
-    id = pId;
-}
-int main ()
-{
-    orang joko;
-    joko.setNama("Joko Susilo");
-    siswa joko_siswa;
-    joko_siswa.setId(1);
-    joko_siswa.displayAll(joko);
-    return 0;
-}
