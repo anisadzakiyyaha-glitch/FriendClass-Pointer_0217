51f59543c4349bc0581558751924ce7ac30e8c6e
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-04-27 18:33:48 +0700

    buat class siswa dengan atribut id

--- FriendClassSatu.cpp
+++ FriendClassSatu.cpp
@@ -10,4 +10,15 @@ string nama;
 
 public:
 void setNama(string pNama);
+friend class siswa;
+};
+
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang &a);
 };
