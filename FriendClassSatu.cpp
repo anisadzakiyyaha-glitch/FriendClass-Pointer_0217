3d0364228b58d7f317868e557e2921fc94dc7f34
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-04-27 18:37:06 +0700

    implementasi fungsi displayAll dan main

--- FriendClassSatu.cpp
+++ FriendClassSatu.cpp
@@ -22,3 +22,11 @@ public:
     void setId(int pId);
     void displayAll(orang &a);
 };
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
