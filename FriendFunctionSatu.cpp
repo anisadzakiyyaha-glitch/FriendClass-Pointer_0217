5be5a0fd96900203123293295b84503bf90e5e30
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-04-27 18:41:37 +0700

    buat class mahasiswa dengan deklarasi friend function setnama

--- FriendFunctionSatu.cpp
+++ FriendFunctionSatu.cpp
@@ -9,15 +9,3 @@ class mahasiswa
     public:
     friend void setnama(mahasiswa &a, string);
 };
-
-void setnama(mahasiswa &a, string b)
-{
-    a.nama = b;
-    cout << a.nama;
-}
-
-int main(){
-    mahasiswa joko;
-    setnama(joko, "kairi kumar");
-    return 0;
-}
